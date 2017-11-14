82779ab168ec433e63183c3f189786a80b30e53b
BinMa <binma@thoughtworks.com>
2017-11-14 22:18:25 +0800

    Add initial code

---
+++ src/com/thoughtworks/Main.java
@@ -0,0 +1,33 @@
+package com.thoughtworks;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Main {
+    private static final SessionFactory ourSessionFactory;
+
+    static {
+        try {
+            ourSessionFactory = new Configuration().
+                configure("hibernate.cfg.xml").
+                buildSessionFactory();
+        } catch (Throwable ex) {
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static Session getSession() throws HibernateException {
+        return ourSessionFactory.openSession();
+    }
+
+    public static void main(final String[] args) throws Exception {
+        final Session session = getSession();
+        try {
+            System.out.println("querying all the managed entities...");
+        } finally {
+            session.close();
+        }
+    }
+}
